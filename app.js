abf34939037638642ff50ed289eb61900eceee51
DipeshRamStha <visit.dipesh933@gmail.com>
2024-03-27 15:50:58 -0500

    Created the base URL as /student in app.use function to provide the relative path to href as css/all.css in index.ejs file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017";
 connectDB(DATABASE_URL);
 
 // Static Files
-app.use(express.static(join(process.cwd(), "public")));
+app.use("/student", express.static(join(process.cwd(), "public")));
 
 // Set Template Engine
 app.set("view engine", "ejs");
