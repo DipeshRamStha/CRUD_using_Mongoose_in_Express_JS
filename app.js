22210d982d532b4b12d81114a366249be141012b
DipeshRamStha <visit.dipesh933@gmail.com>
2024-03-27 14:13:18 -0500

    Loaded the routes, view engine in app.js file and defined the studentController path in the routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./db/connectdb.js";
 import { join } from "path";
+import web from "./routes/web.js";
 const app = express();
 const port = process.env.PORT || "3000";
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017";
@@ -10,6 +11,12 @@ connectDB(DATABASE_URL);
 
 // Static Files
 app.use(express.static(join(process.cwd(), "public")));
+
+// Set Template Engine
+app.set("view engine", "ejs");
+
+// Load Routes
+app.use("/student", web);
 
 // Listening the connection in the 3000 PORT
 app.listen(port, () => {
