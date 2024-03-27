ad888368bf9fb5b21f044e59620ce64bf7a80648
DipeshRamStha <visit.dipesh933@gmail.com>
2024-03-27 13:54:46 -0500

    Linked the fontawesome and bootstrap css and js files in index.ejs file

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import connectDB from "./db/connectdb.js";
+import { join } from "path";
 const app = express();
 const port = process.env.PORT || "3000";
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017";
 
 // Database Connection
 connectDB(DATABASE_URL);
+
+// Static Files
+app.use(express.static(join(process.cwd(), "public")));
 
 // Listening the connection in the 3000 PORT
 app.listen(port, () => {
